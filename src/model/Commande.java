d36cb9e42910bd2afd559a045f8bf42f6b37a32e
Florent DAGNOT <florent.dagnot@laposte.net>
2023-01-30 10:25:46 +0100

    init commande

---
+++ src/model/Commande.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Commande {
+	private int idCommande;
+	private int idClient;
+	private Date date;
+	ArrayList<Ligne> infos;
+	private double prixTotal;
+	
+}
---
+++ src/model/Ligne.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Ligne {
+	private Article a;
+	private int quantite;
+	
+	public Ligne(Article a, int quantite) {
+		this.a = a;
+		this.quantite = quantite;
+	}
+	
+	
+}

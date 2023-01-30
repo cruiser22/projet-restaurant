d86d7972545cc053574101120b196a0c347615ad
unknown <pvinchent@gmail.com>
2023-01-30 08:49:50 +0000

    class accueil + DAO acceuil

---
+++ src/dao/DAOArticle.java
@@ -0,0 +1,60 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Article;
+
+public class DAOArticle {
+  private Connection connection;
+
+  public DAOArticle(Connection connection) {
+    this.connection = connection;
+  }
+
+  public List<Article> getArticles() throws SQLException {
+    List<Article> articles = new ArrayList<>();
+    String sql = "SELECT id, libelle, description, prix FROM articles";
+    PreparedStatement statement = connection.prepareStatement(sql);
+    ResultSet resultSet = statement.executeQuery();
+    while (resultSet.next()) {
+      int id = resultSet.getInt("id");
+      String libelle = resultSet.getString("libelle");
+      String description = resultSet.getString("description");
+      float prix = resultSet.getFloat("prix");
+      articles.add(new Article(id, libelle, description, prix));
+    }
+    return articles;
+  }
+  
+  public void addArticle(Article article) throws SQLException {
+    String sql = "INSERT INTO articles (libelle, description, prix) VALUES (?, ?, ?)";
+    PreparedStatement statement = connection.prepareStatement(sql);
+    statement.setString(1, article.getLibelle());
+    statement.setString(2, article.getDescription());
+    statement.setFloat(3, article.getPrix());
+    statement.executeUpdate();
+  }
+  
+  public void updateArticle(Article article) throws SQLException {
+    String sql = "UPDATE articles SET libelle = ?, description = ?, prix = ? WHERE id = ?";
+    PreparedStatement statement = connection.prepareStatement(sql);
+    statement.setString(1, article.getLibelle());
+    statement.setString(2, article.getDescription());
+    statement.setFloat(3, article.getPrix());
+    statement.setInt(4, article.getId());
+    statement.executeUpdate();
+  }
+  
+  public void deleteArticle(int id) throws SQLException {
+    String sql = "DELETE FROM articles WHERE id = ?";
+    PreparedStatement statement = connection.prepareStatement(sql);
+    statement.setInt(1, id);
+    statement.executeUpdate();
+  }
+}
+
---
+++ src/model/Article.java
@@ -0,0 +1,48 @@
+package model;
+
+public class Article {
+	private int id;
+    private String libelle;
+    private String description;
+    private float prix;
+
+    public Article(int id, String libelle, String description, float prix) {
+        this.id = id;
+        this.libelle = libelle;
+        this.description = description;
+        this.prix = prix;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getPrix() {
+        return prix;
+    }
+
+    public void setPrix(float prix) {
+        this.prix = prix;
+    }
+}
+
